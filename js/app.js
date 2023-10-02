1f3a7a22c92e04c1dcfa312f6b1cfff4ab2a7564
Najeeb Anwari <najeeb.anwari@gmail.com>
2023-10-02 16:27:34 +0430

    Script for for form validation added

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("#form");
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    const email = form.querySelector("#email");
+    const emailFormGroup = email.parentElement;
+    const emailInvalidFeedback  = emailFormGroup.querySelector(".invalid-feedback");
+
+    const emailValue = email.value.trim();
+
+
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isValidEmail = emailRegex.test(emailValue);
+    if(!isValidEmail){
+        emailFormGroup.classList.add("invalid");
+        emailInvalidFeedback.innerHTML = "Please provide a valid email address";
+        return;
+    }
+    else {
+        emailFormGroup.classList.remove("invalid");
+    }
+
+
+    form.submit();  
+
+});
+
